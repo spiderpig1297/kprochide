edb6004dc590f2cdd5829b18e81077237ddd303c
spiderpig1297 <spiderpig1297@gmail.com>
2020-09-25 23:20:34 +0300

    Added debug.sh and some improvments

---
+++ debug.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+trap "" 0 1 2 3 9 13 15
+
+# User configuration
+BUILDROOT_IMAGES_PATH="/home/iofek/workspace/buildroot/output/images"
+VM_USERNAME="root"
+VM_PASSWORD="root"
+KERNEL_MODULE_NAME="kprochide"
+
+CWD=`pwd`
+REMOTE_DIR="/root/"`basename $CWD`
+SSH_PORT=5555
+
+cd $BUILDROOT_IMAGES_PATH
+
+if pgrep "qemu" > /dev/null
+then
+    echo "[+] QEMU is already running, continuing"
+else
+    echo "[+] running QEMU emulation"
+    setsid qemu-system-x86_64 -enable-kvm -cpu host -s -kernel bzImage -m 2048 -hda rootfs.qcow2 -append "root=/dev/sda rw nokaslr" -smp 4 -net nic -net user,hostfwd=tcp::$SSH_PORT-:22 &
+fi
+
+# Busy loop for waiting for the vm to startup and setup ssh
+until sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT -o StrictHostKeyChecking=no -q $VM_USERNAME@localhost exit
+do 
+    echo "[+] waiting for the VM to initialize"
+    sleep 2
+done
+
+echo "[+] VM: initialized"
+
+echo "[+] moving kernel module $KERNEL_MODULE_NAME to the vm"
+sshpass -p "$VM_PASSWORD" scp -P $SSH_PORT -r $CWD $VM_USERNAME@localhost:$REMOTE_DIR
+sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT $VM_USERNAME@localhost "insmod $REMOTE_DIR/$KERNEL_MODULE_NAME.ko"
+
+rm -f ~/.gdbinit
+text_address=`sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT $VM_USERNAME@localhost cat /sys/module/$KERNEL_MODULE_NAME/sections/.text`
+# data_address=`sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT $VM_USERNAME@localhost cat /sys/module/$KERNEL_MODULE_NAME/sections/.data`
+# bss_address=`sshpass -p "$VM_PASSWORD" ssh -p $SSH_PORT $VM_USERNAME@localhost cat /sys/module/$KERNEL_MODULE_NAME/sections/.bss`
+# echo "add-symbol-file $KERNEL_MODULE_NAME.ko $text_address -s .data $data_address -s .bss $bss_address" > ~/.gdbinit
+
+echo "add-symbol-file $KERNEL_MODULE_NAME.ko $text_address" > ~/.gdbinit
+echo "file ./$KERNEL_MODULE_NAME.ko" >> ~/.gdbinit
